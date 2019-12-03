ce57dfbc28f946ebc068db721aa9c2e0357ff56e
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-12-03 15:11:05 +0530

    user management done

---
+++ src/WrldcScadaIssuesPortal/ScadaIssuesPortal.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ScadaIssuesPortal.Data;
+
+[assembly: HostingStartup(typeof(ScadaIssuesPortal.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ScadaIssuesPortal.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ src/WrldcScadaIssuesPortal/ScadaIssuesPortal.Web/Models/UserListVM.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ScadaIssuesPortal.Web.Models
+{
+    public class UserListVM
+    {
+        public List<UserListItemVM> Users { get; set; }
+    }
+}
